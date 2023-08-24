d9eb041c64a035db486d6aa1840f2619d50d8b43
hasibulhasannirob <hasibulnirob987@gmail.com>
2023-08-24 21:32:46 +0600

    Simple Ecommerce Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
